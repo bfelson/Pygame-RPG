040b6c8d376e3c9e76375013850080e86b53a717
Ben <bafelson@gmail.com>
2023-04-19 13:23:43 -0400

    ready for last class

--- config.py
+++ config.py
@@ -1,12 +1,41 @@
-WIN_WIDTH = 800
-WIN_HEIGHT = 400
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
 
-PLAYER_LAYER = 1
+PLAYER_LAYER = 3
+BLOCK_LAYER = 2
+
 PLAYER_SPEED = 1
 
 TILESIZE = 32
 
 RED = (255, 0, 0)
 BLACK = (0,0,0)
+BLUE = (0, 0, 255)
 
 FPS = 60
+
+#n_rows = win_height / tilesize
+#n_columns = win_width / tilesize
+#B = wall, . = nothing, p = player
+
+
+
+
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B..................B',
+    'B..................B',
+    'B....BBB...........B',
+    'B......B...........B',
+    'B......B...........B',
+    'B......B...........B',
+    'B...........P......B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'BBBBBBBBBBBBBBBBBBBB',
+]
